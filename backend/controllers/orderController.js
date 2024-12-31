04a62182d93bd5cb099e40c185a1a119ad941e08
Abdelrahman Khaled <abodzoro10@gmail.com>
2024-12-31 19:43:34 +0200

    update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -45,7 +45,7 @@ const placeOrder = async (req, res) => {
       success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
       cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
     });
-    res.json({ success: true, session_url: frontend_url + "/myorders" });
+    res.json({ success: true, session_url: session.url });
   } catch (error) {
     res.json({ success: false, message: "Error" });
     console.log(error);
